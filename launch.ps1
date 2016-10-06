c5185ca3b07ccf14af4b5c5f14c73391a49e60c9
Mason Herhusky <mherhusk@purdue.edu>
2016-10-05 23:49:57 -0400

    Updated launch.ps1
    added dotnet restore statement

--- launch.ps1
+++ launch.ps1
@@ -1,4 +1,5 @@
 $env:ASPNETCORE_ENVIRONMENT = "Development"
+dotnet restore
 Start-Process -FilePath "dotnet.exe" -ArgumentList "run" -WorkingDirectory "$pwd\src\GMBuddyIdentity"
 Start-Process -FilePath "dotnet.exe" -ArgumentList "run" -WorkingDirectory "$pwd\src\GMBuddyRest"
 Start-Process -FilePath "dotnet.exe" -ArgumentList "run" -WorkingDirectory "$pwd\src\GMBuddyData"
