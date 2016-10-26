6d1e70f86c67e34e688d1e0a385dae57097f1b3f
Garrick Buckley <gbuckle@purdue.edu>
2016-10-25 22:16:20 -0400

    I claim 100% responsibility for this code

--- src/GMBuddy.Rest/Dnd35/Controllers/CampaignsController.cs
+++ src/GMBuddy.Rest/Dnd35/Controllers/CampaignsController.cs
@@ -45,7 +45,7 @@ namespace GMBuddy.Rest.Dnd35.Controllers
                 string userId = User.Claims.Single(c => c.Type.Equals(ClaimTypes.NameIdentifier)).Value;
                 var campaignId = await games.AddCampaignAsync(name, userId);
 
-                return CreatedAtAction(nameof(GetCampaign), new {Id = campaignId}, null);
+                return CreatedAtAction(nameof(GetCampaign), new {Id = campaignId}, new {CampaignId = campaignId});
             }
             catch (Exception e)
             {
