265708c67578c2cd9250099ce0ebc5741ac61f71
Jack Horton <jackpeterhorton@gmail.com>
2016-10-30 16:36:29 -0400

    Added tests, updated circleci, removed old projects from sln

---
+++ test/GMBuddy.Games.Test/Micro20/Campaign/GetCampaigns.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GMBuddy.Games.Micro20;
+using GMBuddy.Games.Micro20.Data;
+using GMBuddy.Games.Micro20.Models;
+using GMBuddy.Games.Test.Micro20.TestUtilities;
+using Xunit;
+
+namespace GMBuddy.Games.Test.Micro20.Campaign
+{
+    public class GetCampaigns
+    {
+        [Fact]
+        public async Task Success()
+        {
+            // Arrange
+            var options = DatabaseSetup.CreateContextOptions();
+            var userId = new Guid();
+            var campaigns = new List<Micro20Campaign>
+            {
+                new Micro20Campaign
+                {
+                    Name = "A campaign",
+                    GmUserId = userId.ToString()
+                }
+            };
+
+            using (var db = new Micro20DataContext(options))
+            {
+                db.Campaigns.AddRange(campaigns);
+                int changes = await db.SaveChangesAsync();
+                Assert.Equal(campaigns.Count, changes);
+            }
+
+            // Act
+            var games = new Micro20GameService(options);
+            var result = await games.GetCampaignsAsync();
+
+            // Assert
+            Assert.Equal(campaigns.Count, result.Count());
+        }
+    }
+}
