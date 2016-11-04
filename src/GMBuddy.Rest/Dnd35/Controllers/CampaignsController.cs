eba31fd5c22e346a0891da3017930f919c49a302
Jack Horton <jackpeterhorton@gmail.com>
2016-11-04 11:03:19 -0400

    Fixing dnd35

--- src/GMBuddy.Rest/Dnd35/Controllers/CampaignsController.cs
+++ src/GMBuddy.Rest/Dnd35/Controllers/CampaignsController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -42,7 +43,7 @@ namespace GMBuddy.Rest.Dnd35.Controllers
         {
             try
             {
-                string userId = User.Claims.Single(c => c.Type.Equals(ClaimTypes.NameIdentifier)).Value;
+                string userId = User.Claims.Single(c => c.Type.Equals(JwtRegisteredClaimNames.Sub)).Value;
                 var campaignId = await games.AddCampaignAsync(name, userId);
 
                 return CreatedAtAction(nameof(GetCampaign), new {Id = campaignId}, new {CampaignId = campaignId});
