f0c73729fc5871e44a0add547696fa815901c179
Jack Horton <jackpeterhorton>
2016-11-14 09:14:56 -0500

    Fix Micro20::GameService::GetCampaigns returning an unserializable campaign model.

---
+++ src/GMBuddy.Games/Micro20/OutputModels/CampaignView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GMBuddy.Games.Micro20.Models;
+
+namespace GMBuddy.Games.Micro20.OutputModels
+{
+    public class CampaignView
+    {
+        private readonly Campaign campaign;
+
+        public CampaignView(Campaign c)
+        {
+            campaign = c;
+        }
+
+        public Guid CampaignId => campaign.CampaignId;
+
+        public string Name => campaign.Name;
+
+        public string GmUserId => campaign.GmUserId;
+
+        public IEnumerable<CharacterSheet> Characters => campaign.Characters?.Select(c => new CharacterSheet(c));
+    }
+}
